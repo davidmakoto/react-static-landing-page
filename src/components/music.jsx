21f53cad650920f13f51921084b2c1ac78165baa
David <davidmacotoward@gmail.com>
2023-03-30 17:44:07 -0700

    mostly finished music section

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -5,16 +5,16 @@ import LouderAlbumCoverSquare from '../assets/louder-album-cover-square.png';
 class Music extends React.Component {
   render() {
     return (
-      <div className="news text-center text-white">
-        <div className="flex justify-center py-8 text-5xl font-bold text-white">
+      <div className="music text-center text-white">
+        <div className="flex justify-center py-8 text-5xl font-bold">
             <h1 className='text-white py-1'>
                 Music
             </h1>
         </div>
         <div className='flex justify-evenly'>
-            <div className='flex flex-col'>
-                <img src={PlacesAlbumSquare} alt={"Image of Lea Michele from the shoulders up, facing sideways with her eyes closed with a slight breeze"} />
-                <div className='flex justify-center py-8 text-5xl font-bold text-white'>
+            <div className='album-places'>
+                <img className="justify-center" src={PlacesAlbumSquare} alt={"Image of Lea Michele from the shoulders up, facing sideways with her eyes closed with a slight breeze"} />
+                <div className='flex justify-center py-8 text-2xl text-white'>
                 <p className='text-white py-1 uppercase'>
                     <a href="url">ITUNES </a>
                     |
@@ -24,9 +24,22 @@ class Music extends React.Component {
                     |
                     <a href="url"> target</a>
                 </p>
+                </div>
             </div>
-        </div>
-          <img src={LouderAlbumCoverSquare} alt={"Image of Lea Michele from the shoulders up, facing sideways with her eyes closed with a slight breeze"} />
+            <div className='album-louder'>
+                <img src={LouderAlbumCoverSquare} alt={"Image of Lea Michele from the shoulders up, facing sideways with her eyes closed with a slight breeze"} />
+                <div className='flex justify-center py-8 text-2xl text-white'>
+                <p className='text-white py-1 uppercase'>
+                    <a href="url">ITUNES </a>
+                    |
+                    <a href="url"> spotify </a>
+                    |
+                    <a href="url"> amazon </a>
+                    |
+                    <a href="url"> target</a>
+                </p>
+                </div>
+            </div>
 
         </div>
 
