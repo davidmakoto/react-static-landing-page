2383b3de87f64e4c6a3d89d22edce15d1c45a813
David <davidmacotoward@gmail.com>
2023-03-31 16:16:02 -0700

    less news button

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -45,6 +45,17 @@ class News extends React.Component {
                     Rewatch my performance on the Late Late show last night here! <br></br>
                     <a href="http://LateLateShow/LeaMichele">http://LateLateShow/LeaMichele</a>
                     </p>
+                    <button
+                    class="inline-block rounded-full border-2 border-neutral-800 px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900 uppercase"
+                    type="button"
+                    data-te-collapse-init
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                    data-te-target="#collapseExample"
+                    aria-expanded="false"
+                    aria-controls="collapseExample">
+                    less news
+                </button>
                 </div>
                 </div>
         </div>
